8ebbfe7dd9e41702ec95bd4aa47867f48926bb80
Rohit Gupta <iamrohit8055@gmail.com>
2026-03-06 21:37:55 +0530

    day 1 done

---
+++ Day-01/Variables and DOM manipulation/Script.js
@@ -0,0 +1,17 @@
+// declaration of variables
+
+let a = 50;  // can be updated but not declared again.    we use this mostly.
+const b = 60;  // can't be updated and declared again.
+var c = 120;   // can be updated and alse declared again.
+
+
+// DOM manipulation
+
+let heading = document.querySelector("h1");
+heading.style.color = "brown";
+
+let bttn = document.querySelector("#button1");
+bttn.style.backgroundColor = "pink";
+
+let para = document.querySelector("#para1");
+para.style.backgroundColor = "magenta";
